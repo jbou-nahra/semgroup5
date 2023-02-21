051b9425762cf602084779e484705de61aaeb4f8
JeromeBurkeIII <jeromeburkeiii@outlook.com>
2023-02-20 22:08:00 -0400

    Created classes for storing of database values.

---
+++ src/main/java/com/napier/sem/Countrylanguage.java
@@ -0,0 +1,37 @@
+package com.napier.sem;
+
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * number of languages  in the world
+ * */
+public class Countrylanguage {
+
+
+    /**
+     * Abbreviated country names
+     * */
+    public String country_code;
+
+    /**
+     * name of language
+     * */
+    public String language;
+
+
+    /**
+     * Is official
+     * */
+    public char is_official;
+
+
+    /**
+     * percentage of person who speak they language
+     * */
+    public String percentage;
+
+
+
+}
