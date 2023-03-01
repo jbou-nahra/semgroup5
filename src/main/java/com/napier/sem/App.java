fb86d2da0cd47b7f90f312709867d32c1800ec24
JeromeBurkeIII <jeromeburkeiii@outlook.com>
2023-03-01 00:11:38 -0400

    Feature 6-9 completed

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -88,23 +88,14 @@ public class App
         // Connect to database
         a.connect();
 
-        // Extract cities information
-        //    ArrayList<City> cities = a.getAllcities();
 
-        //display  cities information
-        // a.printCities(cities);
 
-        /*Report10To13 report10to13 = new Report10To13();
-        report10to13.getReport10(); //produce a report on All the cities in a country organized by largest population to smallest
-        report10to13.getReport11(); // produce a report on All the cities in a district organized by largest population to smallest
-        report10to13.getReport12(); //produce a report on The Top N populated cities in the world where N is provided by the user
-        report10to13.getReport13(); //produce a report on The Top N populated cities in the continent where N is provided by the user
-
-        Report1To5 report1to5 = new Report1To5();
-        report1to5.getReport1();*/
 
         Report6To9 report6To9 = new Report6To9();
-        report6To9.getReport6();
+        report6To9.getReport6();//report on the top N populated countries in a region in the world
+        report6To9.getReport7();// report on All the cities in the world organised by population from the largest to the smallest
+       report6To9.getReport8();// report on All the cities in a continent  in the world by population from largest to smallest
+        report6To9.getReport9();//report on All the cities in a region  in the world organised by population from the largest to the smallest
 
 
         // Disconnect from database
--- src/main/java/com/napier/sem/Report6To9.java
+++ src/main/java/com/napier/sem/Report6To9.java
@@ -24,7 +24,8 @@ public class Report6To9 {
 
         ResultSet rset = null;
         Statement stmt = null;
-        String reportDes = String.format(" report on the top N populated countries in a region in the world ");
+        int limit =3;
+        String reportDes = String.format("report on the top (%o) populated countries in a region in the world ",limit);
         try
         {
             // Create an SQL statement
@@ -66,7 +67,7 @@ public class Report6To9 {
 
         ResultSet rset = null;
         Statement stmt = null;
-        String reportDes = String.format(" report on All the cities in the world organised by population from the largest to the smallest ");
+        String reportDes = String.format("report on All the cities in the world organised by population from the largest to the smallest ");
         try
         {
             // Create an SQL statement
@@ -99,6 +100,94 @@ public class Report6To9 {
 
         }
     }
+
+
+
+    public void getReport8()
+    {
+
+        ResultSet rset = null;
+        Statement stmt = null;
+        String continent = "asia";
+        String reportDes = String.format("report on All the cities in a continent (%s) in the world by population from largest to smallest ",continent);
+        try
+        {
+            // Create an SQL statement
+            stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT city.name, country.name, city.district, city.population  "
+                            + "FROM city "
+                            + "JOIN country on city.countrycode = country.code  "
+                            + "WHERE  country.continent='asia'"
+                            + "ORDER BY population DESC";
+            // Execute SQL statement
+            rset = stmt.executeQuery(strSelect);
+            // Extract city information
+            ArrayList<City> cities = new ArrayList<City>();
+            while (rset.next())
+            {
+                City city = new City();
+                city.city_name = rset.getString("city.name");
+                city.country_name = rset.getString("country.name");
+                city.district = rset.getString("city.district");
+                city.population = rset.getInt("city.population");
+                cities.add(city);
+            }
+            City.printReport(cities, reportDes);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get cities details");
+
+        }
+    }
+
+
+    public void getReport9()
+    {
+
+        ResultSet rset = null;
+        Statement stmt = null;
+        String region = "Southern Europe";
+        String reportDes = String.format("report on All the cities in a region (%s) in the world organised by population from the largest to the smallest",region);
+        try
+        {
+            // Create an SQL statement
+            stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT city.name, country.name, city.district, city.population  "
+                            + "FROM city "
+                            + "JOIN country on city.countrycode = country.code  "
+                            + "WHERE  country.region='Southern Europe'"
+                            + "ORDER BY population DESC";
+            // Execute SQL statement
+            rset = stmt.executeQuery(strSelect);
+            // Extract city information
+            ArrayList<City> cities = new ArrayList<City>();
+            while (rset.next())
+            {
+                City city = new City();
+                city.city_name = rset.getString("city.name");
+                city.country_name = rset.getString("country.name");
+                city.district = rset.getString("city.district");
+                city.population = rset.getInt("city.population");
+                cities.add(city);
+            }
+            City.printReport(cities, reportDes);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get cities details");
+
+        }
+    }
+
+
+
 
 
 }
